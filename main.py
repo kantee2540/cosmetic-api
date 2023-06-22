f0e2813cbd33e48ea2b0ee8ae81d0f93b98d997e
Kantee Charoensedtasin <kantee2540@hotmail.com>
2023-06-22 20:20:30 +0700

    update project

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ app.config["JSON_SORT_KEYS"] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 CORS(app)
 
-
 @app.route('/', methods=["GET"])
 def test():
-    return "<h1>It's work!!</h1>"
+    return {
+        "version": "1.0.0"
+    }
 
 
 @app.route('/profile_img/<path:filename>')
@@ -41,39 +42,39 @@ def base_cosmetic_image(filename):
     return flask.send_from_directory(UPLOAD_FOLDER + "/Images", filename)
 
 
-app.add_url_rule("/api/v1/product", view_func=product)
+app.add_url_rule("/product", view_func=product)
 
-app.add_url_rule("/api/v1/beautyset", view_func=beauty_set)
-app.add_url_rule("/api/v1/beautyset/<topic_id>", view_func=show_beauty_set)
-app.add_url_rule("/api/v1/beautyset/my_set/save", view_func=save_beauty_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set/delete", view_func=delete_beauty_save_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set/is_liked", view_func=beauty_check_liked, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set/set_like", view_func=set_like, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set/set_unlike", view_func=set_unlike, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set/saved_set", view_func=get_beauty_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/my_set", view_func=get_my_beauty_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/add", view_func=add_beauty_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/delete", view_func=delete_beauty_set, methods=["POST"])
-app.add_url_rule("/api/v1/beautyset/edit", view_func=edit_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset", view_func=beauty_set)
+app.add_url_rule("/beautyset/<topic_id>", view_func=show_beauty_set)
+app.add_url_rule("/beautyset/my_set/save", view_func=save_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset/my_set/delete", view_func=delete_beauty_save_set, methods=["POST"])
+app.add_url_rule("/beautyset/my_set/is_liked", view_func=beauty_check_liked, methods=["POST"])
+app.add_url_rule("/beautyset/my_set/set_like", view_func=set_like, methods=["POST"])
+app.add_url_rule("/beautyset/my_set/set_unlike", view_func=set_unlike, methods=["POST"])
+app.add_url_rule("/beautyset/my_set/saved_set", view_func=get_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset/my_set", view_func=get_my_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset/add", view_func=add_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset/delete", view_func=delete_beauty_set, methods=["POST"])
+app.add_url_rule("/beautyset/edit", view_func=edit_beauty_set, methods=["POST"])
 
-app.add_url_rule("/api/v1/brand", view_func=brand)
-app.add_url_rule("/api/v1/categories", view_func=categories)
-app.add_url_rule("/api/v1/user", view_func=get_user_profile, methods=["POST"])
-app.add_url_rule("/api/v1/user/create", view_func=create_user, methods=["POST"])
-app.add_url_rule("/api/v1/user/update", view_func=update_user, methods=["POST"])
-app.add_url_rule("/api/v1/user/delete", view_func=delete_user, methods=["POST"])
-app.add_url_rule("/api/v1/user/upload_profile", view_func=upload_profile, methods=["POST"])
+app.add_url_rule("/brand", view_func=brand)
+app.add_url_rule("/categories", view_func=categories)
+app.add_url_rule("/user", view_func=get_user_profile, methods=["POST"])
+app.add_url_rule("/user/create", view_func=create_user, methods=["POST"])
+app.add_url_rule("/user/update", view_func=update_user, methods=["POST"])
+app.add_url_rule("/user/delete", view_func=delete_user, methods=["POST"])
+app.add_url_rule("/user/upload_profile", view_func=upload_profile, methods=["POST"])
 
-app.add_url_rule("/api/v1/cosmetic_desk/check", view_func=check_product_is_saved, methods=["POST"])
-app.add_url_rule("/api/v1/cosmetic_desk/add", view_func=add_product_to_desk, methods=["POST"])
-app.add_url_rule("/api/v1/cosmetic_desk/delete", view_func=delete_product_from_desk, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/check", view_func=check_product_is_saved, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/add", view_func=add_product_to_desk, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/delete", view_func=delete_product_from_desk, methods=["POST"])
 
-app.add_url_rule("/api/v1/cosmetic_desk/list", view_func=get_cosmetic_desk, methods=["POST"])
-app.add_url_rule("/api/v1/cosmetic_desk/favorite", view_func=get_cosmetic_desk_favorite, methods=["POST"])
-app.add_url_rule("/api/v1/cosmetic_desk/update_favorite", view_func=update_favorite, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/list", view_func=get_cosmetic_desk, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/favorite", view_func=get_cosmetic_desk_favorite, methods=["POST"])
+app.add_url_rule("/cosmetic_desk/update_favorite", view_func=update_favorite, methods=["POST"])
 
-app.add_url_rule("/api/v1/brand", view_func=brand)
-app.add_url_rule("/api/v1/categories", view_func=categories)
+app.add_url_rule("/brand", view_func=brand)
+app.add_url_rule("/categories", view_func=categories)
 
 if __name__ == '__main__':
     app.run()
