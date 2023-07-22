3bd1493b2a5c8ac882ab6ddec27bd3d201f877d4
Kantee Charoensedtasin <kantee2540@hotmail.com>
2023-07-22 15:39:42 +0700

    Config file

--- connection.py
+++ connection.py
@@ -1,11 +1,17 @@
 import pymysql
+import yaml
 
 
 def connect_db():
-    connection = pymysql.connect(host="localhost",
-                                 user="root",
-                                 password="",
-                                 db="cosmetic-mobile",
+
+    cfg = {}
+    with open('config.yaml') as config_file:
+        cfg = yaml.load(config_file, Loader=yaml.FullLoader)['database']
+
+    connection = pymysql.connect(host=cfg['host'],
+                                 user=cfg['user'],
+                                 password=cfg['password'],
+                                 db=cfg['db'],
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor,
                                  autocommit=True)
